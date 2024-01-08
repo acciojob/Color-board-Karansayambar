1ac0dda3f3459207acf5e8c1aba2167806c4cfbd
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-01-08 22:59:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded" function() {
+	
+})
+const container = document.getElementById("container");
+    const boxCount = 800;
+function createBox() {
+	for (let i = 0; i < boxCount; i++) {
+      const box = document.createElement("div");
+      box.className = "square";
+      container.appendChild(box);
+    }
+}
+createBox();
+)};
